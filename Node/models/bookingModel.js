4153da104126cbef4eba95eeaefc9f20147f97a6
ramez Raed <ramez.20375422@compit.aun.edu.eg>
2024-06-20 18:47:55 +0300

    TourContext&&bookingModel

--- Node/models/bookingModel.js
+++ Node/models/bookingModel.js
@@ -1,11 +1,37 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const bookingSchema = new mongoose.Schema({
-  tourId: { type: mongoose.Schema.Types.ObjectId, ref: "Tour" },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  date: Date,
+  tour: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Tour',
+    required: [true, 'Review must belong to a tour.']
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: [true, 'Review must belong to a user']
+  },
+  place: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Place',
+    required: [true, 'Review must belong to a place']
+  },
+  date: { Date, required: [true, 'Review can not be empty!'] },
+  bookingState: {
+    type: String,
+    enum: ['Confirmed', 'Pending', 'Canceled'],
+    default: 'Pending'
+  },
+  paymentStatus: { type: String, enum: ['Paid', 'Unpaid'], default: 'Unpaid' },
+  paymentAmount: { type: Number },
+  paymentMethod: { type: String, enum: ['Credit Card', 'PayPal', 'Cash'] },
+  numberOfParticipants: { type: Number, default: 1 },
+  specialRequests: { type: String },
+  cancellationAllowed: { type: Boolean, default: true },
+  cancellationDeadline: Date,
+  statusUpdates: [{ status: String, date: Date }]
 });
 
-const Booking = mongoose.model("Booking", bookingSchema);
+const Booking = mongoose.model('Booking', bookingSchema);
 
 module.exports = Booking;
